60025e7bc53a5c2efdfefc1d30cf4b7de8f54402
Aaryan Bhagat <bhagataaryan2004@gmail.com>
2023-09-09 22:51:39 +0530

    Added in Drawer and made some minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,57 @@ void main() {
               appBar: AppBar(
                 backgroundColor: Colors.lightGreenAccent,
                 centerTitle: true,
-                title: Text('This is title of the AppBar', style: TextStyle(
-                  color: Colors.redAccent, fontWeight: FontWeight.bold
-                ),)
+                title: const Text('This is title of the AppBar',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold
                 ),
-            body: Center(
-              child: Text("Hey there! This is my first Flutter App", style: TextStyle(
-                fontSize: 36, color: Colors.white
-              ),),
+                ),
+                ),
+            body:const Center(
+              child: Text("Hey there! This is my first Flutter App",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'IndieFlower',
+                  // fontWeight: FontWeight.bold,
+                  // fontSize: 24,
+              ),
+              ),
             ),
             floatingActionButton: FloatingActionButton(
               onPressed: () {},
-              child: Text('Click me'),
+              backgroundColor: Colors.lightGreenAccent,
+              child: const Text('B1', style: TextStyle(color: Colors.black,),),
+            ),
+            // floatingActionButtonAnimator:FloatingActionButtonAnimator(
+            drawer: Drawer(
+              backgroundColor: Colors.green,
+              child: ListView(
+                // padding: EdgeInsets.zero,
+
+                children: const <Widget>[
+                  DrawerHeader(
+                  curve: Curves.easeIn,
+                  duration: Duration(milliseconds: 1000),
+                  margin: EdgeInsets.zero,
+                  child: Text('Drawer Header', style: TextStyle(color: Colors.deepPurple, fontSize: 12),),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.home),
+                    title: Center(
+                      child: Text('List Tile 1'),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Center(
+                      child: Text('List Tile 2'),
+                    ),
+                  )
+                ],
+              ),
+            ),
             ),
             )
-  ));
+  );
 }
